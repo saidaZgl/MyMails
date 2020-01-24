ac32ce405e060c9ad90cd5a92eb7adcfd72c8c48
Saida Zghal <zghal.saida@orange.fr>
2020-01-24 18:44:32 +0100

    Capter le clic de l’utilisateur sur le picto poubelle

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ for (let i = 0; i < document.getElementsByClassName("trash").length; i++) {
   document
     .getElementsByClassName("trash")
     [i].addEventListener("click", function() {
-      console.log(this, "click détecté");
+      console.log(this);
     });
 }
