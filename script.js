b9446b93b0781cf05e114b6cc9e7a793ac47b64a
Saida Zghal <zghal.saida@orange.fr>
2020-01-25 15:40:48 +0100

    mise en place d'une mécanique permettant de supprimer un message lorsque l’on clique sur son picto poubelle

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ for (let i = 0; i < document.getElementsByClassName("trash").length; i++) {
   document
     .getElementsByClassName("trash")
     [i].addEventListener("click", function() {
-      console.log(this);
+      this.parentNode.remove();
+      messageCount = document.getElementById("count").textContent;
     });
 }
