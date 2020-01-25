4e58a225fed54f8a2a4a15714e26b7c962f21739
Saida Zghal <zghal.saida@orange.fr>
2020-01-25 16:11:56 +0100

    MàJ du compteur

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let messagesCount = document.getElementsByTagName("p").length;
-messageCount = document.getElementById("count").textContent;
+document.getElementById("count").textContent = messagesCount;
+//console.log(messagesCount);
 
 for (let i = 0; i < document.getElementsByClassName("trash").length; i++) {
   document
     .getElementsByClassName("trash")
     [i].addEventListener("click", function() {
       this.parentNode.remove();
-      messageCount = document.getElementById("count").textContent;
+      let messagesCount = document.getElementsByTagName("p").length;
+      document.getElementById("count").textContent = messagesCount;
     });
 }
