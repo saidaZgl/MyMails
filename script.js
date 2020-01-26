96c97a152da790b4c86e9fb77207f2d7ed44ba93
Saida Zghal <zghal.saida@orange.fr>
2020-01-26 15:58:25 +0100

    mise en place d'un champ de saisie pour rédiger un message

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ for (let i = 0; i < document.getElementsByClassName("trash").length; i++) {
       document.getElementById("count").textContent = messagesCount;
     });
 }
+
+document.getElementById("btn-add").addEventListener("click", function() {
+  //console.log(document.getElementById("add-message").value);
+
+  let mainDiv = document.createElement("div");
+  mainDiv.className = "row";
+  document.body.appendChild(mainDiv);
+
+  let image = document.createElement("img");
+  image.className = "avatar";
+  image.src = "images/avatar-2.jpg";
+  mainDiv.appendChild(image);
+
+  let nameMessageDiv = document.createElement("div");
+  mainDiv.appendChild(nameMessageDiv);
+
+  let h6 = document.createElement("h6");
+  h6.textContent = "Saïda ZGHAL";
+  nameMessageDiv.appendChild(h6);
+
+  let content = document.createElement("p");
+  content.textContent = document.getElementById("add-message").value;
+  nameMessageDiv.appendChild(content);
+
+  let trash = document.createElement("img");
+  trash.className = "trash";
+  trash.src = "images/trash.png";
+  mainDiv.appendChild(trash);
+
+  let messagesCount = document.getElementsByTagName("p").length;
+  document.getElementById("count").textContent = messagesCount;
+
+  trash.addEventListener("click", function() {
+    this.parentNode.remove();
+    let messagesCount = document.getElementsByTagName("p").length;
+    document.getElementById("count").textContent = messagesCount;
+  });
+});
